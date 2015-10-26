5853733d34b83fadf249dc5334e7fd788b590281
Andrew Silluron <asilluron@users.noreply.github.com>
2015-10-26 12:49:06 -0700

    Sample Config. Uses public beta key for testing

---
+++ app/config.sample.js
@@ -0,0 +1,3 @@
+//DO NOT COMMIT BACK TO GITHUB
+angular.module('pi').constant('API_KEY', 'dc6zaTOxFJmzC'); // TEST API KEY ONLY - PUBLIC BETA KEY
+angular.module('pi').constant('API_URL', 'http://api.giphy.com/v1/gifs');
