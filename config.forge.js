56ff2dc2c6229f312fb81b9a1de12882bac844b2
Andrew Silluron <asilluron@users.noreply.github.com>
2022-04-12 09:46:58 -0600

    Fix up config for latest version, fix notarize script

--- config.forge.js
+++ config.forge.js
@@ -1,53 +1,54 @@
 config = {
-  icon: 'assets/icons/icon.icns',
-  appBundleId: 'com.silluron.gifcake',
-  osxSign: true
-}
+  icon: "assets/icons/icon.icns",
+  appBundleId: "com.silluron.gifcake",
+  osxSign: true,
+};
 
 config.ignore = (file) => {
   if (!file) return false;
 
   return !/^[/\\]\.webpack($|[/\\]).*$/.test(file);
-}
+};
 
 module.exports = {
-  buildIdentifier: 'com.silluron.gifcake',
+  buildIdentifier: "com.silluron.gifcake",
   packagerConfig: config,
-  "mac": {
-    "hardenedRuntime": true
+  mac: {
+    hardenedRuntime: true,
+    gatekeeperAssess: false,
+    entitlements: "build/entitlements.mac.plist",
+    entitlementsInherit: "build/entitlements.mac.plist",
   },
   makers: [
     {
       name: "@electron-forge/maker-dmg",
       config: {
-        background: './assets/dmg-background.png',
+        background: "./assets/dmg-background.png",
         format: "ULFO",
-        icon: './assets/icons/icon.icns'       
-      }
+        icon: "./assets/icons/icon.icns",
+      },
     },
     {
-      "name": "@electron-forge/maker-zip",
-      "platforms": [
-        "darwin"
-      ]
-    }
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin"],
+    },
   ],
   plugins: [
     [
       "@electron-forge/plugin-webpack",
       {
-        "mainConfig": "./webpack.main.config.js",
-        "renderer": {
-          "config": "./webpack.renderer.config.js",
-          "entryPoints": [
+        mainConfig: "./webpack.main.config.js",
+        renderer: {
+          config: "./webpack.renderer.config.js",
+          entryPoints: [
             {
-              "html": "./src/index.html",
-              "js": "./src/App.tsx",
-              "name": "main_window"
-            }
-          ]
-        }
-      }
-    ]
-  ]
-}
+              html: "./src/index.html",
+              js: "./src/App.tsx",
+              name: "main_window",
+            },
+          ],
+        },
+      },
+    ],
+  ],
+};
